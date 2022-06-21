c464d9cd1c6f236031a78d17c6cc8763e58d3dc8
Sarbojit Das <sarbojit@skymarc.it.uu.se>
2022-06-21 15:51:49 +0200

    More bechmarks

--- benchmarks/event-driven/branching/consensus.c
+++ benchmarks/event-driven/branching/consensus.c
@@ -5,7 +5,7 @@
 
 #ifndef N
 #  warning "N was not defined; defining it as 2"
-#  define N 3
+#  define N 2
 #endif
 
 typedef struct {
@@ -17,7 +17,6 @@ qthread_t collectors[N];
 
 atomic_int decision[N];
 atomic_int value[N];
-param p[N][N];
 atomic_int a[N];
 
 void send_value(void *par) {
@@ -25,17 +24,22 @@ void send_value(void *par) {
   if (val > decision[(*(param *)par).i]) {
     decision[(*(param *)par).i] = val;
   }
+  return 0;
 }
 
-void *handler_func(void *j){ 
+void *broadcast(void *j) {
   atomic_store_explicit(&value[*(atomic_int *)j], *(atomic_int *)j, memory_order_seq_cst);
   
   for (int i = 0; i < N; i++) {
-    int k = *(int *)j;
-    p[i][k].i = i;
-    p[i][k].j = k;
-    qthread_post_event(collectors[i], &send_value, &p[i][k]);
+    param p;
+    p.i = i;
+    p.j = *(int *)j;
+    qthread_post_event(collectors[i], &send_value, &p);
   }
+  return 0;
+}
+
+void *handler_func(void *i){ 
   int quit = qthread_exec();
   return 0;
 }
@@ -44,13 +48,23 @@ int main() {
   for (int i = 0; i < N; i++) {
     decision[i] = -1;
   }
-
+  
   for (int i = 0; i < N; i++) {
-    a[i] = i;
-    qthread_create(&collectors[i], &handler_func, &a[i]);
+    qthread_create(&collectors[i], &handler_func, NULL);
     qthread_start(collectors[i]);
   }
- 
+  
+  pthread_t t[N];
+  
+  for (int i = 0; i < N; i++) {
+    a[i] = i;
+    pthread_create(&t[i], NULL, &broadcast, &a[i]);
+  }
+  
+  for (int i = 0; i < N; i++) {
+    pthread_join(t[i], NULL);
+  }
+  
   for (int i = 0; i < N; i++) {
     qthread_wait(collectors[i], NULL);
   }
