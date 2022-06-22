5bd62af5b67fc82951729bfe6afd2d476a9c9243
Sarbojit Das <sarbojit@debianblue>
2022-06-22 12:07:27 +0200

    Fix chang_roberts benchmark

--- benchmarks/event-driven/branching/chang-roberts.c
+++ benchmarks/event-driven/branching/chang-roberts.c
@@ -17,53 +17,55 @@ qthread_t nodes[N];
 
 atomic_int ids[N];
 atomic_int decision[N];
-int a = 0;
+atomic_int a[N];
+param p[N];
+param p2[N][N];
 
 void announce(void *par) {
-  param p = *(param *)par;
-  
+  atomic_int p_own = (*(param *)par).own;
+  atomic_int p_new = (*(param *)par).new;
+
   // write the index of the leader node
-  atomic_store_explicit(&decision[p.own], p.new, memory_order_seq_cst);
+  atomic_store_explicit(&decision[p_own], p_new, memory_order_seq_cst);
 }
 
 void msg(void *par) {
-  param p = *(param *)par;
+  atomic_int p_own = (*(param *)par).own;
+  atomic_int p_new = (*(param *)par).new;
   
-  if (p.own == p.new) {
+  if (p_own == p_new) {
     // if a node receives its own id then it will announce to all nodes it is the leader
     for (int i = 0; i < N; i++) {
-      param p2;
-      p2.own = i;
-      p2.new = p.own;
-      qthread_post_event(nodes[i], &announce, &p2);
+      p2[p_own][i].own = i;
+      p2[p_own][i].new = p_own;
+      qthread_post_event(nodes[i], &announce, &p2[p_own][i]);
     }
   } else {
-    atomic_int own_val = atomic_load_explicit(&ids[p.own], memory_order_seq_cst);
-    atomic_int new_val = atomic_load_explicit(&ids[p.new], memory_order_seq_cst);
+    atomic_int own_val = atomic_load_explicit(&ids[p_own], memory_order_seq_cst);
+    atomic_int new_val = atomic_load_explicit(&ids[p_new], memory_order_seq_cst);
     if (new_val > own_val) {
       // if a node receives an id greater than its own, replace its own id and forward the new id
-      atomic_store_explicit(&ids[p.own], new_val, memory_order_seq_cst);
-      param p2;
-      p2.own = (p.own + 1) % N;
-      p2.new = p.new;
-      qthread_post_event(nodes[(p.own + 1) % N], &msg, &p2);
-    }  
+      atomic_store_explicit(&ids[p_own], new_val, memory_order_seq_cst);
+      p2[p_own][(p_own+1)%N].own = (p_own + 1) % N;
+      p2[p_own][(p_own+1)%N].new = p_new;
+      qthread_post_event(nodes[(p_own + 1) % N], &msg, &p2[p_own][(p_own+1)%N]);
+    }
   }
 }
 
 // initial message to make all nodes start sending messages
 void init_msg(void *par) {
   int i = *(int *)par;
-  param p;
-  p.own = (i + 1) % N;
-  p.new = i;
-  qthread_post_event(nodes[(i+1) % N], &msg, &p);
+  p[i].own = (i + 1) % N;
+  p[i].new = i;
+  qthread_post_event(nodes[(i+1) % N], &msg, &p[i]);
 }
 
 // initializes all nodes by making them send a messages to their neighbor
 void *init(void *par) {
   for (int i = 0; i < N; i++) {
-    qthread_post_event(nodes[i], &init_msg, &i);
+    a[i] = i;
+    qthread_post_event(nodes[i], &init_msg, &a[i]);
   }
   return 0;
 }
@@ -83,11 +85,8 @@ int main() {
   
   pthread_t t;
   
-  pthread_create(&t, NULL, &init, &a);
+  pthread_create(&t, NULL, &init, NULL);
   
   pthread_join(t, NULL);
-  
-  for (int i = 0; i < N; i++) {
-    qthread_wait(nodes[i], NULL);
-  }
+  return 0;
 }
