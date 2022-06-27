90bc8a271de06f1b9e50f08fb80c93dbbb8cbe72
Magnus Lång <margnus1@telia.com>
2022-06-27 13:33:17 +0000

    Add documentation to qthread.h (#3)

--- benchmarks/event-driven/qthread.h
+++ benchmarks/event-driven/qthread.h
@@ -4,36 +4,73 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+/* This header implements a compatibility layer, allowing a qthread
+ * program to run either on qthreads or on pthreads.
+ * When running on pthreads, qthread handlers are modelled as mutexes,
+ * and posting an event instead creates a thread that acquires the mutex
+ * before executing the message payload.
+ *
+ * The model is designed with benchmarking fairness as a primary goal.
+ * Even when running on qthreads, the benchmark will make almost the
+ * same sequence of events, except omitting mutex lock/unlock, and
+ * performing qthread_post_event instead of pthread_create.
+ *
+ * When running on qthreads, the real qthreads functions (not the ones
+ * we redirect to with ifdefs) will be intercepted by nidhugg (or other
+ * event-aware tool), instead of calling the stubs we have in this file.
+ *
+ * The header is also designed to be a drop-in replacement for the
+ * "standard" qthread header, using #defines to redirect calls to
+ * qthread functions to its own.
+ */
+
+/* The native qthread handler type */
+typedef unsigned long int _actual_qthread_t;
+
+/* The handler type that is exposed by our compatibility layer */
 typedef struct {
   pthread_mutex_t mutex;
-  pthread_t qthread;
+  _actual_qthread_t qthread;
   int is_pthread;
 } *qthread_t;
 
-typedef struct{
+/* The payload type passed to our messages */
+struct _msg_arg {
   void (*func)(void *);
   void *arg;
   qthread_t tid;
-} msg_arg_t;
+};
 
-int qthread_create(pthread_t *tid, void *(*func)(void *), void * arg){
-  return 1;
+#define _MAGIC_I_AM_NOT_INTERCEPTED -42
+
+int qthread_create(_actual_qthread_t *tid, void *(*func)(void *), void * arg){
+  return _MAGIC_I_AM_NOT_INTERCEPTED;
 }
-int qthread_create_internal(qthread_t *tid, void *(*func)(void *), void * arg){
+static int qthread_create_internal(qthread_t *tid, void *(*func)(void *), void * arg){
   *tid = malloc(sizeof(**tid));
   int ret = pthread_mutex_init(&(*tid)->mutex,NULL);
-  (*tid)->is_pthread = qthread_create(&(*tid)->qthread, func, arg);
-  return ret;
+  if (ret) return ret;
+  ret = qthread_create(&(*tid)->qthread, func, arg);
+  (*tid)->is_pthread = (ret == _MAGIC_I_AM_NOT_INTERCEPTED);
+  if ((*tid)->is_pthread) return 0;
+  else return ret;
 }
-void qthread_wait_internal(qthread_t tid, void *ret_val){
-  /* Empty; we choose not to model wait */
+
+static void qthread_wait_internal(qthread_t tid, void *ret_val){
+  /* Empty; we choose not to model wait
+   *
+   * Note that this means that wait will not wait but return
+   * immediately.
+   */
 }
-void qthread_start(pthread_t tid){}
-void qthread_start_internal(qthread_t tid){
+
+void qthread_start(_actual_qthread_t tid){}
+static void qthread_start_internal(qthread_t tid){
   qthread_start(tid->qthread);
 }
-void *msg_func(void *_msg_arg){
-  msg_arg_t *msg_arg = (msg_arg_t *)_msg_arg;
+
+static void *_pthread_func(void *_msg_arg){
+  struct _msg_arg *msg_arg = (struct _msg_arg *)_msg_arg;
   void (*func)(void *) = msg_arg->func;
   void *arg = msg_arg->arg;
   pthread_mutex_t *mutex = &msg_arg->tid->mutex;
@@ -43,8 +80,9 @@ void *msg_func(void *_msg_arg){
   free(msg_arg);
   return NULL;
 }
-void *post_func(void *_msg_arg){
-  msg_arg_t *msg_arg = (msg_arg_t *)_msg_arg;
+static void *_post_func(void *_msg_arg){
+  /* Replicate the actions of _pthread_func as much as possible */
+  struct _msg_arg *msg_arg = (struct _msg_arg *)_msg_arg;
   void (*func)(void *) = msg_arg->func;
   void *arg = msg_arg->arg;
   pthread_mutex_t *mutex = &msg_arg->tid->mutex;
@@ -54,21 +92,24 @@ void *post_func(void *_msg_arg){
   free(msg_arg);
   return NULL;
 }
-void qthread_post_event(pthread_t tid, void (*func)(void *), void *arg){
-}
-void qthread_post_event_internal(qthread_t tid, void (*func)(void *), void *arg){
+void qthread_post_event(_actual_qthread_t tid, void (*func)(void *), void *arg);
+static void qthread_post_event_internal(qthread_t tid, void (*func)(void *), void *arg){
   pthread_t msg;
-  msg_arg_t *msg_arg = malloc(sizeof(msg_arg_t));
+  struct _msg_arg *msg_arg = malloc(sizeof(struct _msg_arg));
   msg_arg->func = func;
   msg_arg->arg = arg;
   msg_arg->tid = tid;
   if (tid->is_pthread)
-    pthread_create(&msg,NULL,&msg_func,msg_arg);
+    pthread_create(&msg,NULL,_pthread_func,msg_arg);
   else
-    qthread_post_event(tid->qthread,post_func,msg_arg);
+    qthread_post_event(tid->qthread,_post_func,msg_arg);
 }
-int qthread_exec(){return 0;}
 
+int qthread_exec(){
+  return 0;
+}
+
+/* Redirect calls to these qthreads functions */
 #define qthread_create qthread_create_internal
 #define qthread_start qthread_start_internal
 #define qthread_post_event qthread_post_event_internal
