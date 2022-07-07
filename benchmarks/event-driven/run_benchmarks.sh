769ba9b8076a991d8638abdaacc7188de388c7c7
Sarbojit Das <sarbojit@debianblue>
2022-07-07 15:15:49 +0200

    Change the name of the bechmarks according to the text

--- benchmarks/event-driven/run_benchmarks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-alias LS='ls -l'
-alias NIDHUGGC='/home/sarbojit/local/nidhugg/src/nidhuggc -sc -event'
-for FILE in *.c
-do
-  echo "****************************************************************************************************"
-  LS $FILE
-  NIDHUGGC $FILE
-done
