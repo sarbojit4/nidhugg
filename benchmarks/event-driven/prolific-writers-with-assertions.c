f4537cdccada5849c5a52176c05c18a5e02d905d
Sarbojit Das <sarbojit@debianblue>
2023-02-02 14:15:20 +0100

    Restore correct version of prolific-writers-with-assertions

--- benchmarks/event-driven/prolific-writers-with-assertions.c
+++ benchmarks/event-driven/prolific-writers-with-assertions.c
@@ -11,21 +11,24 @@
 #endif
 
 qthread_t handler;
-atomic_int a[(N+1)*N];
-atomic_int count[(N+1)*N];
+atomic_int a[N*N];
+atomic_int counter[N*N];
 atomic_int id[N];
+atomic_int msg_finished[N];
 
 void __VERIFIER_assume(intptr_t);
 
 void mes(void *_id){
   atomic_int num = *(atomic_int *)_id;
-  for (atomic_int i = num*N; i < N+num*N; i++){
+  for (atomic_int i = num*N; i <= (N+num*N-1)%(N*N); i++){
     atomic_store_explicit(&a[i], 2, memory_order_seq_cst);
+    atomic_store_explicit(&counter[i], counter[i]+1, memory_order_seq_cst);
   }
-  for (atomic_int i = N+num*N; i < 2*N+num*N; i++){
+  for (atomic_int i = (N+num*N)%(N*N); i <= (2*N+num*N-1)%(N*N); i++){
     atomic_int b = atomic_load_explicit(&a[i], memory_order_seq_cst);
+    atomic_store_explicit(&counter[i], counter[i]+1, memory_order_seq_cst);
   }
-  id[N-1] = 0;
+  msg_finished[num] = 1;
 }
 
 void *handler_func(void *i){ 
@@ -38,6 +41,12 @@ int main(){
   qthread_start(handler);
   for (int i = 0; i < N; i++){
     id[i] = i;
+    msg_finished[i] = 0;
+  }
+  for (int i = 0; i < N*N; i++) counter[i] = 0;
+  for (int i = 0; i < N; i++){
     qthread_post_event(handler, &mes, &id[i]); 
   }
+  for(int i=0; i<N; i++) __VERIFIER_assume(msg_finished[i] == 1);
+  for(int i=0; i<N*N; i++) assert(counter[i] == 2);
 }
