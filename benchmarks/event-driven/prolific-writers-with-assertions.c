e455173d048efff06546184f517c68fd0329d8d3
Sarbojit Das <sarbojit@debianblue>
2023-02-04 19:27:30 +0100

    Move assertion from main therad to msgs in prolific

--- benchmarks/event-driven/prolific-writers-with-assertions.c
+++ benchmarks/event-driven/prolific-writers-with-assertions.c
@@ -29,6 +29,8 @@ void mes(void *_id){
     atomic_store_explicit(&counter[i], counter[i]+1, memory_order_seq_cst);
   }
   msg_finished[num] = 1;
+  for (atomic_int i = (N+num*N)%(N*N); i <= (2*N+num*N-1)%(N*N); i++)
+    assert(msg_finished[num] == 0 || msg_finished[(num+1)%N] == 0 || counter[i] == 2);
 }
 
 void *handler_func(void *i){ 
@@ -47,6 +49,6 @@ int main(){
   for (int i = 0; i < N; i++){
     qthread_post_event(handler, &mes, &id[i]); 
   }
-  for(int i=0; i<N; i++) __VERIFIER_assume(msg_finished[i] == 1);
-  for(int i=0; i<N*N; i++) assert(counter[i] == 2);
+  //for(int i=0; i<N; i++) __VERIFIER_assume(msg_finished[i] == 1);
+  //for(int i=0; i<N*N; i++) assert(counter[i] == 2);
 }
